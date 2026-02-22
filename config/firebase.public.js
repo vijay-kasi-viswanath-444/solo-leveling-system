3d10be2d4a352d60f8efb8510e37da7cb9d8f582
vijay-kasi-viswanath-444 <vijaykasiviswanath@gmail.com>
2026-02-22 19:36:33 +0530

    first commit

---
+++ config/firebase.public.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD2ttn5JjhFE-SfhImKezYKhtd2UjGVoLI",
+  authDomain: "solo-leveling-system-adf64.firebaseapp.com",
+  projectId: "solo-leveling-system-adf64",
+  storageBucket: "solo-leveling-system-adf64.firebasestorage.app",
+  messagingSenderId: "480144591802",
+  appId: "1:480144591802:web:4ae8c6385d7e8e5b85eb83",
+  measurementId: "G-BJZ4XCKQZQ"
+};
